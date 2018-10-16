297913d777b3f607ab4a8f5d041484a4004c0f1f
R Jay <15009097+J4CODE@users.noreply.github.com>
2018-10-15 22:01:17 -0400

    Update carts.js
    Updated JS

--- routes/carts.js
+++ routes/carts.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const router  = express.Router();
-const User    = require('../models/user.js');
-const Cart    = require('../models/cart.js')
+const router = express.Router();
+const User = require('../models/user.js');
+const Cart = require('../models/cart.js')
 const Product = require('../models/product.js')
 
 
