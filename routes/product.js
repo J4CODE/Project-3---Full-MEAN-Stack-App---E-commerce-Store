a8cddfdaddd2962e08c709a3faec1bd1989bdb1a
R Jay <15009097+J4CODE@users.noreply.github.com>
2018-10-15 22:03:19 -0400

    Update product.js
    Updated Products JS

--- routes/product.js
+++ routes/product.js
@@ -1,32 +1,38 @@
-const express       = require('express');
+const express = require('express');
 const productRouter = express.Router();
-const Product       = require('../models/product.js')
+const Product = require('../models/product.js')
 
 
 //----------------------------------------------------------------------------------
 //This GETS ALL THE PRODUCTS, that have been SAVED TO THE DATABASE in MongoDB Compass
 
-productRouter.get('/product', function(req, res){
+productRouter.get('/product', function (req, res) {
   Product.find()
-  .then(ellproducts=>{
-    res.json(ellproducts)
-  }).catch(err=>{res.json(err)})
-    // .exec(function(err, products){
-    //   console.log(products)
-    //   if(err) console.log(err);
-    //   console.log('this is the error',err)
-    //   console.log('Products Samples From the Seeds Database', products)
-    //   res.json({products})
-    // })
+    .then(ellproducts => {
+      res.json(ellproducts)
+    }).catch(err => {
+      res.json(err)
+    })
+  // .exec(function(err, products){
+  //   console.log(products)
+  //   if(err) console.log(err);
+  //   console.log('this is the error',err)
+  //   console.log('Products Samples From the Seeds Database', products)
+  //   res.json({products})
+  // })
 })
 
 //-----------------------------------------------------------------------------
 //This gets a product detail
 
-productRouter.get('/product/:id', (req, res, next)=>{
-  Product.findById(req.params.id, (err, response)=>{
-    if (err)    { return res.json(err).status(500); }
-    if (respose) { return res.json(err).status(404),console.log(err) }
+productRouter.get('/product/:id', (req, res, next) => {
+  Product.findById(req.params.id, (err, response) => {
+    if (err) {
+      return res.json(err).status(500);
+    }
+    if (respose) {
+      return res.json(err).status(404), console.log(err)
+    }
 
     return res.json(response);
   })
@@ -36,7 +42,7 @@ productRouter.get('/product/:id', (req, res, next)=>{
 //-----------------------------------------------------------------------------
 //This TAKES THE DATA FROM the FRONT END FORM and CREATES a new product.
 
-productRouter.post('/create/product', function(req, res){
+productRouter.post('/create/product', function (req, res) {
   const newProduct = new Product({
     name: req.body.name,
     description: req.body.description,
@@ -46,45 +52,47 @@ productRouter.post('/create/product', function(req, res){
     feedback: req.body.feedback
   })
 
-  newProduct.save(function(err, newProduct){
-    console.log(newProduct);
-    if(err) console.log(err);
+  newProduct.save(function (err, newProduct) {
+      console.log(newProduct);
+      if (err) console.log(err);
 
-    console.log('Yay! A New product has been created in product post route: ', newProduct);
-    // res.json({newProduct});
-  })
-  .then(response => {
-    console.log("product created successfully ------------ ", response);
-    res.status(200).json(response)
-  })
-  .catch(err => {
-    console.log("error creating product ==================== ", err);
-    res.status(400).json({message: "Error creating product"})
-  })
+      console.log('Yay! A New product has been created in product post route: ', newProduct);
+      // res.json({newProduct});
+    })
+    .then(response => {
+      console.log("product created successfully ------------ ", response);
+      res.status(200).json(response)
+    })
+    .catch(err => {
+      console.log("error creating product ==================== ", err);
+      res.status(400).json({
+        message: "Error creating product"
+      })
+    })
 })
 
 //-------------------------------------------------------------------------------------------------
 //This EDITS a specific product
 
-productRouter.post('/product/:id/update', (req, res, next)=>{
+productRouter.post('/product/:id/update', (req, res, next) => {
   const theId = req.params.id;
   Product.findByIdAndUpdate(theId, {
-    description: req.body.description,
-    name: req.body.name,
-    img: req.body.img,
-    price: req.body.price,
-    quantity: req.body.quantity,
-    feedback: req.body.feedback
-  })
+      description: req.body.description,
+      name: req.body.name,
+      img: req.body.img,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      feedback: req.body.feedback
+    })
 
-  .then((response)=>{
-    console.log('this is the response',response);
-    res.json(response)
-  })
-  .catch((err)=>{
-    console.log('this is the erros',err);
-    res.json(err);
-  })
+    .then((response) => {
+      console.log('this is the response', response);
+      res.json(response)
+    })
+    .catch((err) => {
+      console.log('this is the erros', err);
+      res.json(err);
+    })
 })
 
 //-----------------------------------------------------
@@ -92,17 +100,13 @@ productRouter.post('/product/:id/update', (req, res, next)=>{
 //This DELETES a speciic product.
 productRouter.post('/product/:id/delete', (req, res, next) => {
   Product.findByIdAndRemove(req.params.id)
-  .then((response) => {
-   res.json(response);
-  })
-  .catch((err) => {
-   res.json(err);
-  })
- })
-
-
-
-
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((err) => {
+      res.json(err);
+    })
+})
 
 
 module.exports = productRouter;
